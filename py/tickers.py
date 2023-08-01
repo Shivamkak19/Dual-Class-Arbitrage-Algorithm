48554b9ecf50299e11cedf005bfc94ca2461f1ef
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2023-07-31 23:19:30 -0500

    Set up tickers.py for retrieving HTTP get requests, set update function for import params

--- py/tickers.py
+++ py/tickers.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import requests
+import datetime
 
 # List for each dual-class share
 class tickers():
 
-    # Initialize dual-class shares
+    # Initialize all columns of stock_df dataset
     def __init__(self):
+
+        # Avoid path truncation
+        pd.options.display.max_colwidth = 300
         
-        self.l2 = ["ARTNA", "ARTNB"]
-        self.l3 = ["BELFA", "BELFB"]
-        self.l4 = ["BRK.A", "BRK.B"]
-        self.l5 = ["BIO", "BIO.B"]
-        self.l6 = ["BF.A", "BF.B"]
-        self.l7 = ["STZ", "STZ.B"]
-        self.l8 = ["FOXA", "FOX"]
-        self.l1 = ["GOOGL", "GOOG"]
-        self.l9 = ["GEF", "GEF.B"]
-        self.l10 = ["LEN", "LEN.B"]
+        self.stock_df = pd.read_csv("../shares.csv")
+        self.stock_df.columns = ["class_A", "class_B"]
 
-        # parent list for all chares
-        self.parent = [self.l1, self.l2, self.l3, self.l4, self.l5, self.l6, self.l7, self.l8, self.l9, self.l10]
-        self.stock_df = pd.DataFrame(self.parent, columns=["class_A", "class_B"])
+        column_names = ["API_path_A", "open_price_A", "current_price_A", "update_time_A", "json_A", 
+                        "API_path_B", "open_price_B", "current_price_B", "update_time_B", "json_B",
+                        "open_price_margin", "current_price_margin"]
+        
+        # Init columns to null values
+        self.stock_df[column_names] = None
+        self.stock_df["position"] = False
+
         self.setAPIPath()
+        self.updateValues()
 
+    # Set API links upon initialization
     def setAPIPath(self):
 
         api_auth = "257ae70a8408461284a148458eb2717f"
@@ -45,24 +48,111 @@ class tickers():
             path += api_auth
             classB_list.append(path)
 
-        # Avoid path truncation
-        pd.options.display.max_colwidth = 300
+        df = pd.DataFrame({"API_Path_A": classA_list})
+        self.stock_df.update(df)
 
-        self.stock_df.insert(loc = 1,
-                column = 'class_A_API_PATH',
-                value = classA_list)
+        df = pd.DataFrame({"API_Path_B": classB_list})
+        self.stock_df.update(df)
+    
+    # Set opening values for trading day
+    def dailyOpen(self):
 
-        self.stock_df.insert(loc = 3,
-                column = 'class_B_API_PATH',
-                value = classB_list)
+        open_A = []
+        open_B = []
+        open_margin = []
+
+        for link in self.stock_df["API_path_A"]:
+            r = requests.get(link).json()
+            price_A = r["previous_close"]
+            open_A.append(price_A)
+        
+        for link in self.stock_df["API_path_B"]:
+            r = requests.get(link).json()
+            price_B = r["previous_close"]
+            open_B.append(price_B)
+
+
+        for i in range(len(open_A)):
+            margin = abs(open_A[i] - open_B[i])
+            open_margin.append(margin)
+
+        df = pd.DataFrame({"open_price_A": open_A, 
+                           "open_price_B": open_B, 
+                           "open_price_margin": open_margin})
+        
+        self.stock_df.update(df)
+
+    
+    # Update values of stock_df periodically
+    def updateValues(self):
+
+        jsonList_A = []
+        currentPrice_A = []
+        updateTime_A = []
+
+        jsonList_B = []
+        currentPrice_B = []
+        updateTime_B = []
+
+        currentMargin = []
+
+
+        # Access JSON via HTTP request, convert to Python dictionary
+        # Access values via dict
+        for link in self.stock_df["API_path_A"]:
+            r = requests.get(link).json()
+            jsonList_A.append(r)
+            
+            price_A = r["price"]
+            time_A = r["timestamp"]
+
+            # convert from unix time to datetime
+            time_A = datetime.datetime.fromtimestamp(time_A)
+
+            updateTime_A.append(time_A)
+            currentPrice_A.append(price_A)
+
+        for link in self.stock_df["API_path_B"]:
+            r = requests.get(link).json()
+            jsonList_B.append(r)
+
+            price_B = r["price"]
+            time_B = r["timestamp"]
+
+            # convert from unix time to datetime
+            time_B = datetime.datetime.fromtimestamp(time_B)
+
+            updateTime_B.append(time_B)
+            currentPrice_B.append(price_B)
+
+
+        # Set Current Margin values
+        for i in range(len(currentPrice_A)):
+            margin = abs(currentPrice_A[i] - currentPrice_B[i])
+            currentMargin.append(margin)
+
+
+        # Update all values in stock_df
+        df = pd.DataFrame({"json_A": jsonList_A, 
+                           "json_B": jsonList_B, 
+                           "current_price_A": currentPrice_A,
+                           "current_price_B" : currentPrice_B,
+                           "update_time_A" : updateTime_A,
+                           "update_time_B" : updateTime_B,
+                           "current_price_margin": currentMargin})
+        
+        self.stock_df.update(df)
+
 
     def getStock(self):
         return self.stock_df
 
+
 # Concatonate
 
-
-ticker = tickers()
-df = ticker.getStock()
-print(df.head(10))
+test = tickers()
+df = test.getStock()
+test.setJSON()
+print(df.head())
+test.setJSON()
 
